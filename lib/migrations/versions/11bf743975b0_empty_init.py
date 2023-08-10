4f44a191542fb53ad92bb942a6ebc09b4cf1a897
Stephen Csengo <stephencsengo@gmail.com>
2023-08-10 16:48:58 -0400

    Codealong complete

---
+++ lib/migrations/versions/11bf743975b0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 11bf743975b0
+Revises: 
+Create Date: 2023-08-10 16:18:47.480810
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '11bf743975b0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
